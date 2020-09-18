c43a151585323c4f1134d62cc491a1cc0ed5dc2e
maja26 <43316150+maja26@users.noreply.github.com>
2020-09-18 11:23:25 +0200

    create project

---
+++ server/vnote/src/main/java/com/vnote/vnote/VnoteApplication.java
@@ -0,0 +1,13 @@
+package com.vnote.vnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnoteApplication.class, args);
+	}
+
+}
---
+++ server/vnote/src/test/java/com/vnote/vnote/VnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnote.vnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
