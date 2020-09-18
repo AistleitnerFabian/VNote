63338ccd96662dad0957cd7aa1cb25037b1a1ec1
maja26 <43316150+maja26@users.noreply.github.com>
2020-09-18 12:23:29 +0200

    import opencv

--- server/vnote/src/main/java/com/server/vnote/vnote/VNoteApplication.java
+++ server/vnote/src/main/java/com/server/vnote/vnote/VNoteApplication.java
@@ -2,12 +2,14 @@ package com.server.vnote.vnote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import nu.pattern.OpenCV;
 
 @SpringBootApplication
 public class VNoteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VNoteApplication.class, args);
+		OpenCV.loadLocally();
 	}
 
 }
---
+++ server/vnote/src/main/java/com/server/vnote/vnote/recognition/PostitRecognition.java
@@ -0,0 +1,11 @@
+package recognition;
+
+import com.server.vnote.vnote.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PostitRecognition{
+    
+}
