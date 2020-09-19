d8b2a2d750fef908a2c33dc47cb89abdbc25e00c
AistleitnerFabian <fabian.aistleitner@gmail.com>
2020-09-19 19:59:47 +0200

    webclient angular init

---
+++ client/vnote-client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
