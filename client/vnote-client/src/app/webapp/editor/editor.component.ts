6b8b9116cbd8ea6708f16b8e26d2670f9c774905
AistleitnerFabian <56301447+AistleitnerFabian@users.noreply.github.com>
2020-09-24 17:53:14 +0200

    panzoom infinite grid + profil menu layer correction

--- client/vnote-client/src/app/webapp/editor/editor.component.ts
+++ client/vnote-client/src/app/webapp/editor/editor.component.ts
@@ -1,14 +1,21 @@
-import {Component, OnInit} from '@angular/core';
-import {PanZoomConfig} from "ngx-panzoom";
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {PanZoomConfig, PanZoomAPI, PanZoomModel} from 'ngx-panzoom';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss']
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements OnInit, OnDestroy, AfterViewInit {
 
   panZoomConfig: PanZoomConfig;
+  private panZoomAPI: PanZoomAPI;
+  private apiSubscription: Subscription;
+  private modelChangedSubscription: Subscription;
+  gridSize = 150;
+  @ViewChild('grid') grid;
+  @ViewChild('panzoom') panzoom;
 
   constructor() {
     this.panZoomConfig = new PanZoomConfig();
@@ -22,6 +29,25 @@ export class EditorComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.apiSubscription = this.panZoomConfig.api.subscribe((api: PanZoomAPI) => this.panZoomAPI = api);
+    this.modelChangedSubscription = this.panZoomConfig.modelChanged.subscribe((model: PanZoomModel) => this.onModelChanged(model));
   }
 
+  ngOnDestroy(): void {
+    this.apiSubscription.unsubscribe();  // don't forget to unsubscribe.  you don't want a memory leak!
+    this.modelChangedSubscription.unsubscribe();  // don't forget to unsubscribe.  you don't want a memory leak!
+  }
+
+  ngAfterViewInit(): void {
+    this.grid.nativeElement.style.backgroundSize = this.panzoom.scale * this.gridSize + 'px';
+  }
+
+  onModelChanged(model: PanZoomModel): void {
+    if (this.grid !== undefined) {
+      console.log(this.panzoom.scale);
+      this.grid.nativeElement.style.backgroundPositionX = model.pan.x + 'px';
+      this.grid.nativeElement.style.backgroundPositionY = model.pan.y + 'px';
+      this.grid.nativeElement.style.backgroundSize = this.panzoom.scale * this.gridSize + 'px';
+    }
+  }
 }
