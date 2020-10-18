7c417b6a62c2c270628a28b9d90e08e81ce48988
AistleitnerFabian <56301447+AistleitnerFabian@users.noreply.github.com>
2020-10-18 15:48:55 +0200

    custom drag and drop

---
+++ client/vnote-client/src/app/webapp/editor/drag-and-drop.service.ts
@@ -0,0 +1,17 @@
+import {HostListener, Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DragAndDropService {
+
+  isDraging: boolean = false;
+  dragX = 0;
+  dragY = 0;
+  dragHandler: HTMLElement;
+  scale = 1;
+
+  constructor() {
+  }
+
+}
