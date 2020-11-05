a20e4b347a72a9db6bdf1579a46c9445e5d87214
AistleitnerFabian <56301447+AistleitnerFabian@users.noreply.github.com>
2020-11-05 22:41:27 +0100

    zoomTo working / grid solution for auto zoom

--- client/vnote-client/src/app/webapp/editor/editor.component.ts
+++ client/vnote-client/src/app/webapp/editor/editor.component.ts
@@ -1,13 +1,22 @@
-import {AfterViewInit, Component, HostListener, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  setTestabilityGetter,
+  ViewChild
+} from '@angular/core';
 import {PanZoomConfig, PanZoomAPI, PanZoomModel} from 'ngx-panzoom';
 import {Subscription} from 'rxjs';
 import {DragAndDropService} from './drag-and-drop.service';
 import {Note} from './model/note';
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.scss']
+  styleUrls: ['./editor.component.scss'],
 })
 export class EditorComponent implements OnInit, OnDestroy, AfterViewInit {
 
@@ -19,6 +28,8 @@ export class EditorComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild('grid') grid;
   @ViewChild('panzoom') panzoom;
   noteArray = [];
+  gridVisible = true;
+  focused = {x: null, y: null};
 
   constructor(private dragAndDropService: DragAndDropService) {
     this.panZoomConfig = new PanZoomConfig();
@@ -50,19 +61,40 @@ export class EditorComponent implements OnInit, OnDestroy, AfterViewInit {
 
   zoomTo(posX, posY): void {
     try {
-      this.panZoomAPI.zoomToFit({x: posX - 250, y: posY - 250, width: 800, height: 800}, 0.35);
+      if (this.focused.x !== posX && this.focused.y !== posY) {
+        this.gridVisible = false;
+        this.panZoomAPI.config.panOnClickDrag = false;
+        this.panZoomAPI.zoomToFit({x: posX - 250, y: posY - 250, width: 800, height: 800}, 0.35);
+        setTimeout(() => {
+          this.gridVisible = true;
+          this.setGridPosition(this.panZoomAPI.model);
+          this.focused.x = posX;
+          this.focused.y = posY;
+          this.panZoomAPI.config.panOnClickDrag = true;
+        }, 350);
+      }
     } catch (e) {
+      console.error(e);
     }
   }
 
+  resetFocus(): void {
+    this.focused.x = null;
+    this.focused.y = null;
+  }
+
   onModelChanged(model: PanZoomModel): void {
-    if (this.grid !== undefined) {
+    this.resetFocus();
+    this.setGridPosition(model);
+  }
+
+  setGridPosition(model: PanZoomModel): void {
+    if (this.gridVisible && this.grid !== undefined) {
       this.grid.nativeElement.style.backgroundPositionX = model.pan.x + 'px';
       this.grid.nativeElement.style.backgroundPositionY = model.pan.y + 'px';
       this.grid.nativeElement.style.backgroundSize = this.panzoom.scale * this.gridSize + 'px';
     }
   }
-
 
   @HostListener('mouseup')
   onMouseup(): void {
